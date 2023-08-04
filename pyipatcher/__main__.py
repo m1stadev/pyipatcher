026a60aeb3b3e0fe0955df8266527f2d3d71c95c
m1stadev <adamhamdi31@gmail.com>
2023-08-04 14:23:02 -0500

    update CLI

--- pyipatcher/__main__.py
+++ pyipatcher/__main__.py
@@ -2,14 +2,23 @@ import click
 from .cli.ramdiskpatcher import ramdiskpatcher
 from .cli.kernelpatcher import kernelpatcher
 from .cli.ibootpatcher import ibootpatcher
+import sys
+import coloredlogs
+import logging
+
+coloredlogs.install(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @click.group()
 def cli():
-    pass
-   
+    sys.tracebacklimit = 0
+
+
+cli.context_settings = dict(help_option_names=['-h', '--help'])
+
 cli.add_command(ramdiskpatcher)
 cli.add_command(kernelpatcher)
 cli.add_command(ibootpatcher)
 
-
-cli.context_settings = dict(help_option_names=['-h', '--help'])
+if __name__ == '__main__':
+    cli()
